40f7f1ed8e13a9f1865f19dfd2d834990224d558
Saradhi888 <vijay.saradhi888@gmail.com>
2016-05-12 00:08:34 +0530

    Add files via upload

--- MemberReg.java
+++ MemberReg.java
@@ -1,5 +1,40 @@
 package libraryManagement;
 
 public class MemberReg {
+	private int Id;
+	private String Name,Password,Email,Branch,Gender;
+	
+	public int Id(){
+		return Id;
+	}
+	public String Name(){
+		return Name;
+	}
+	public String Password(){
+		return Password;
+	}
+	public String Email(){
+		return Email;
+	}
+	
+	public void setId(int Id){
+		this.Id=Id;
+	}
+	public void Name(String Name){
+		this.Name=Name;
+	}
+	public void setEmail(String Email){
+		this.Email=Email;
+	}
+	public void setPassword(String Password){
+		this.Password=Password;
+	}
+	public void setBranch(String Branch){
+		this.Branch=Branch;
+	}
+	public void setGender(String Gender){
+		this.Gender=Gender;
+	}
+	
 
 }
